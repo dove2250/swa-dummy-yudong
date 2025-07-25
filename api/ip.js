bbcd6c75692909c93c0a798969680ec2bd09e6d4
dove2250 <138220659+dove2250@users.noreply.github.com>
2025-07-25 10:35:07 +0900

    Delete api directory

--- api/ip.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  const forwarded = req.headers["x-forwarded-for"] || req.headers["x-client-ip"] || req.ip || "unknown";
-  context.res = {
-    status: 200,
-    body: `Request source IP: ${forwarded}`
-  };
-};
